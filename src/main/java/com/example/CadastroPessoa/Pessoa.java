9e41453be669b3c0a5d920ec2e44adc876723f76
Kynan02 <kaynanuriel18@gmail.com>
2025-04-26 18:17:06 -0300

    cadastro

---
+++ src/main/java/com/example/CadastroPessoa/Pessoa.java
@@ -0,0 +1,53 @@
+package com.example.CadastroPessoa;
+
+public class Pessoa {
+
+    String nome;
+    String genero;
+    String telefone;
+    String endereco;
+    Integer idade;
+    
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getGenero() {
+        return this.genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public Integer getIdade() {
+        return this.idade;
+    }
+
+    
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return this.endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+    
+    public String getTelefone() {
+        return this.telefone;
+    }
+  
+      
+}
